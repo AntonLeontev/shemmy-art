7a187475d8dbf4117d7039d75d3593bd27c8fd8a
Anton <aner-anton@yandex.ru>
2024-03-26 19:33:12 +0300

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Роспись одежды на заказ | Shemmy Art</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="min-h-screen font-sans antialiased">
+    <div class="flex h-full w-full">
+        123654
+        <div class=""></div>
+    </div>
+</body>
+
+</html>
